741ea46bf541a7698046a403e67cbbd315af13f2
Ronin <muteki.ronin@gmail.com>
2022-10-08 19:47:35 +0300

    add github pages and secret API key

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -5,6 +5,8 @@ import Movies from "../components/Movies";
 import Preloader from "../components/Preloader";
 import Search from "../components/Search";
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 class Main extends React.Component {
   state = {
     movies: [],
@@ -12,7 +14,7 @@ class Main extends React.Component {
   };
 
   componentDidMount() {
-    fetch("https://www.omdbapi.com/?apikey=7e1c4674&s=batman")
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=batman`)
       .then((response) => response.json())
       .then((data) => this.setState({ movies: data.Search, loading: false }))
       .catch((error) => {
@@ -24,7 +26,7 @@ class Main extends React.Component {
   searchMovies = (src, type = "all") => {
     this.setState({ loading: true });
     fetch(
-      `https://www.omdbapi.com/?apikey=7e1c4674&s=${src}${
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${src}${
         type !== "all" ? `&type=${type}` : ""
       }`
     )
