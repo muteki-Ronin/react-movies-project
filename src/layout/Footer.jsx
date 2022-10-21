66a0997e971ff2de0f9e7d70b892f1d3fb3aa73f
Ronin <muteki.ronin@gmail.com>
2022-10-21 15:11:55 +0300

    fix link

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,13 +1,21 @@
 function Footer() {
-  return <footer className="page-footer light-blue lighten-1">
-    <div className="footer-copyright">
-      <div className="container">
-        © {new Date().getFullYear()} Copyright Text
-        <a className="grey-text text-lighten-4 right" href="https://github.com/muteki-Ronin/react-movies-project">Repo</a>
+  return (
+    <footer className="page-footer light-blue lighten-1">
+      <div className="footer-copyright">
+        <div className="container">
+          © {new Date().getFullYear()} Copyright Text
+          <a
+            className="grey-text text-lighten-4 right"
+            href="https://github.com/muteki-Ronin/react-movies-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Repo
+          </a>
+        </div>
       </div>
-    </div>
-  </footer>
-  
+    </footer>
+  );
 }
 
 export default Footer;
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,13 +1,27 @@
 function Header() {
-  return <nav className="light-blue lighten-1">
-    <div className="nav-wrapper">
-      <a href="https://muteki-Ronin.github.io/react-movies-project" className="brand-logo">React Movies</a>
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="https://github.com/muteki-Ronin/react-movies-project">Repo</a></li>
-      </ul>
-    </div>
-  </nav>
-
+  return (
+    <nav className="light-blue lighten-1">
+      <div className="nav-wrapper">
+        <a
+          href="https://muteki-Ronin.github.io/react-movies-project"
+          className="brand-logo"
+        >
+          React Movies
+        </a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <a
+              href="https://github.com/muteki-Ronin/react-movies-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Repo
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
